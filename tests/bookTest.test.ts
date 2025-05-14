65d8179de7e04ed11f35a85bcf7a0c2d070f1d58
Dante-eraa <eziokenwayearth@gmail.com>
2025-05-14 19:38:52 +0530

    Add test

--- tests/bookTest.test.ts
+++ tests/bookTest.test.ts
@@ -1,8 +1,8 @@
 import request from "supertest";
-import { app } from "../src/server"; // Path to your server
+import { app } from "../src/server";
 
 describe("Book API", () => {
-  // Test for creating a new book
+  // Testing fot create a new book
   it("should create a new book", async () => {
     const newBook = {
       title: "Test Book",
@@ -12,8 +12,8 @@ describe("Book API", () => {
     const response = await request(app).post("/books").send(newBook);
 
     expect(response.status).toBe(201);
-    expect(response.body).toHaveProperty("id"); // Book should have an id
-    expect(response.body.title).toBe(newBook.title); // Title should match
-    expect(response.body.author).toBe(newBook.author); // Author should match
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.title).toBe(newBook.title);
+    expect(response.body.author).toBe(newBook.author);
   });
 });
